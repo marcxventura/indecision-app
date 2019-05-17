2eead5e0e1dc226bd71dcba9c53545370a51ad55
Marc Ventura <marcxventura@gmail.com>
2019-05-16 17:03:58 -0700

    conditional rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,76 @@
 
 console.log("App.js is running!");
 
-// JSX - Javascipt XML
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
+};
+
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision App"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  app.options.length > 0 ? "Here are your options:" : "No options",
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+var user = {
+  name: "Marc Ventura",
+  age: 35,
+  location: "Los Angeles"
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : "Anonymous"
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
