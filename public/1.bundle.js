a401d5c9ee46c4a88615a74c37ac57805476c5fd
Marc Ventura <marcxventura@gmail.com>
2019-06-24 19:12:18 -0700

    installed webpack, setup webpack config file, and explored ES6 import/export

---
+++ public/1.bundle.js
@@ -0,0 +1,33 @@
+webpackJsonp([1],{
+
+/***/ 2:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+console.log("person.js is running!!!");
+
+const isAdult = age => {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+};
+/* harmony export (immutable) */ __webpack_exports__["isAdult"] = isAdult;
+
+
+const canDrink = age => {
+  if (age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+};
+/* harmony export (immutable) */ __webpack_exports__["canDrink"] = canDrink;
+
+
+
+/***/ })
+
+});
---
+++ src/person.js
@@ -0,0 +1,17 @@
+console.log("person.js is running!!!");
+
+export const isAdult = age => {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const canDrink = age => {
+  if (age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+};
