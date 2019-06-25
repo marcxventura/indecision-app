45956b91a8895a60c6c5c84341303c9ad094959f
Marc Ventura <marcxventura@gmail.com>
2019-06-24 19:22:21 -0700

    explored default exports

--- public/bundle.js
+++ public/bundle.js
@@ -69,49 +69,39 @@
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
-// import { square, add } from "./utils";
-
-// console.log("app.js is running!!");
-// console.log(square(6));
-// console.log(add(3, 5));
-
-// person.js
-//named export isADult
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils__ = __webpack_require__(1);
 
 
+console.log("app.js is running!!");
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils__["c" /* square */](6));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils__["a" /* add */](3, 5));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils__["b" /* default */](9, 3));
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
+// import { isAdult, canDrink } from "./person.js";
+
+// console.log(isAdult(17));
+// console.log(canDrink(21));
 
 
 /***/ }),
-/* 1 */,
-/* 2 */
+/* 1 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-console.log("person.js is running!!!");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return subtract; });
+console.log("utils.js is running");
 
-const isAdult = age => {
-  if (age >= 18) {
-    return true;
-  } else {
-    return false;
-  }
-};
-/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+const square = x => x * x;
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
 
 
-const canDrink = age => {
-  if (age >= 21) {
-    return true;
-  } else {
-    return false;
-  }
-};
-/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
+// exports - default export - named exports
 
 
 /***/ })
--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-// import { square, add } from "./utils";
+import subtract, { square, add } from "./utils";
 
-// console.log("app.js is running!!");
-// console.log(square(6));
-// console.log(add(3, 5));
+console.log("app.js is running!!");
+console.log(square(6));
+console.log(add(3, 5));
+console.log(subtract(9, 3));
 
-// person.js
-//named export isADult
+// import { isAdult, canDrink } from "./person.js";
 
-import { isAdult, canDrink } from "./person.js";
-
-console.log(isAdult(17));
-console.log(canDrink(21));
+// console.log(isAdult(17));
+// console.log(canDrink(21));
