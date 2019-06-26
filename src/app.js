e74356cdf50cb61330c83ed7d9d2ef26f7c9d36c
Marc Ventura <marcxventura@gmail.com>
2019-06-25 17:15:23 -0700

    imported react and react-dom npm modules

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// import subtract, { square, add } from "./utils";
+import React from "react";
+import ReactDOM from "react-dom";
 
-// console.log("app.js is running!!");
-// console.log(square(6));
-// console.log(add(3, 5));
-// console.log(subtract(9, 3));
-
-import isSenior, { isAdult, canDrink } from "./person.js";
-
-console.log(isAdult(17));
-console.log(canDrink(21));
-console.log(isSenior(75));
+const template = React.createElement("p", {}, "testing 123");
+ReactDOM.render(template, document.getElementById("app"));
